0e02b5421102d1b708ecd051b5335f588892a0e6
jiagege <1366790651@qq.com>
2024-06-25 17:28:56 +0800

    第二次提交

--- src/main/java/com/itheima/Hello.java
+++ src/main/java/com/itheima/Hello.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello.main");
+        System.out.println("Hello,test2");
     }
 }
